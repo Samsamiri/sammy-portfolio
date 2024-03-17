54945e6c7d9f7d19c310f2a839b98888424f5ebe
sammy <a.sammy.samiri@gmail.com>
2024-03-17 14:32:44 +0100

    z

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/project_3D_developer_portfolio', // Remplacez <repo> par le nom de votre dépôt GitHub
   build: {
     outDir: 'build', // Spécifiez le répertoire de sortie souhaité ici
   },
